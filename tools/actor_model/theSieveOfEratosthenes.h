1c0935c5e5f817a72219bed948ac36a636017e9e
utsx <dimma1506@icloud.com>
2023-06-09 22:45:49 +0300

    Implemented: TReadActor, TMaximumPrimeDevisorActor, TWriteActor. Added theSieveOfEratosthenes class.

---
+++ tools/actor_model/theSieveOfEratosthenes.h
@@ -0,0 +1,37 @@
+//
+// Created by postgres2 on 09.06.23.
+//
+
+#ifndef ACTOR_MODEL_THESIEVEOFERATOSTHENES_H
+#define ACTOR_MODEL_THESIEVEOFERATOSTHENES_H
+
+class TheSieveOfEratosthenes {
+private:
+    std::vector<bool> prime;
+public:
+    TheSieveOfEratosthenes() {
+//        int n = std::numeric_limits<int>::max() - 100;
+        int n = 10;
+        prime.resize(n + 1, true);
+        prime[0] = prime[1] = false;
+        for (int i = 2; i <= n; i++) {
+            if (prime[i])
+                if(i * 1ll * i <= n)
+                    for(int j = i * i; j <=n; j += i)
+                        prime[j] = false;
+        }
+    }
+    void toString(){
+        for(int i = 1; i <= 10; i++){
+            std::cout << prime[i] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    bool checkPrime(int value){
+        return prime[value];
+    }
+};
+
+
+#endif //ACTOR_MODEL_THESIEVEOFERATOSTHENES_H
