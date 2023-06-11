f55e10892202ccbb7105d0d70e94abd8349367fd
xopose <30976652+xopose@users.noreply.github.com>
2023-06-12 00:23:11 +0300

    Update actors.h

--- tools/actor_model/actors.h
+++ tools/actor_model/actors.h
@@ -3,6 +3,9 @@
 #include <util/generic/ptr.h>
 #include <library/cpp/actors/util/should_continue.h>
 
+THolder<NActors::IActor> CreateTReadActor(const NActors::TActorId writeActor);
+THolder<NActors::IActor> CreateTMaximumPrimeDevisorActor(int64_t value, const NActors::TActorIdentity readActor, const NActors::TActorId writeActor);
+THolder<NActors::IActor> CreateTWriteActor();
 THolder<NActors::IActor> CreateSelfPingActor(const TDuration& latency);
 
 std::shared_ptr<TProgramShouldContinue> GetProgramShouldContinue();
