3b3715a91743c35b64d90665ba5178aaa5f08160
debugByPrintln1 <melnikov.n.89@mail.ru>
2023-06-12 04:05:59 +0300

    td

--- tools/actor_model/main.cpp
+++ tools/actor_model/main.cpp
@@ -4,33 +4,36 @@
 #include <util/generic/xrange.h>
 
 THolder<NActors::TActorSystemSetup> BuildActorSystemSetup(ui32 threads, ui32 pools) {
-    auto setup = MakeHolder<NActors::TActorSystemSetup>();
-    setup->ExecutorsCount = pools;
-    setup->Executors.Reset(new TAutoPtr<NActors::IExecutorPool>[pools]);
-    for (ui32 idx : xrange(pools)) {
-        setup->Executors[idx] = new NActors::TBasicExecutorPool(idx, threads, 512);
-    }
-    setup->Scheduler.Reset(new NActors::TBasicSchedulerThread(NActors::TSchedulerConfig(512, 0)));
-    return setup;
+  auto setup = MakeHolder<NActors::TActorSystemSetup>();
+  setup->ExecutorsCount = pools;
+  setup->Executors.Reset(new TAutoPtr<NActors::IExecutorPool>[pools]);
+  for (ui32 idx : xrange(pools)) {
+    setup->Executors[idx] = new NActors::TBasicExecutorPool(idx, threads, 512);
+  }
+  setup->Scheduler.Reset(new NActors::TBasicSchedulerThread(NActors::TSchedulerConfig(512, 0)));
+  return setup;
 }
 
 int main(int argc, const char* argv[])
 {
-    Y_UNUSED(argc, argv);
-    auto actorySystemSetup = BuildActorSystemSetup(20, 1);
-    NActors::TActorSystem actorSystem(actorySystemSetup);
-    actorSystem.Start();
+  Y_UNUSED(argc, argv);
+  auto actorySystemSetup = BuildActorSystemSetup(20, 1);
+  NActors::TActorSystem actorSystem(actorySystemSetup);
+  actorSystem.Start();
 
-    actorSystem.Register(CreateSelfPingActor(TDuration::Seconds(1)).Release());
+  actorSystem.Register(CreateSelfPingActor(TDuration::Seconds(1)).Release());
 
-    // Зарегистрируйте Write и Read акторы здесь
+  // Зарегистрируйте Write и Read акторы здесь
 
-    // Раскомментируйте этот код
-    // auto shouldContinue = GetProgramShouldContinue();
-    // while (shouldContinue->PollState() == TProgramShouldContinue::Continue) {
-    //     Sleep(TDuration::MilliSeconds(200));
-    // }
-    actorSystem.Stop();
-    actorSystem.Cleanup();
-    // return shouldContinue->GetReturnCode();
+  NActors::TActorId writer = actorSystem.Register(CreateTWriteActor().Release());
+  actorSystem.Register(CreateTReadActor(writer).Release());
+
+  // Раскомментируйте этот код
+  auto shouldContinue = GetProgramShouldContinue();
+  while (shouldContinue->PollState() == TProgramShouldContinue::Continue) {
+    Sleep(TDuration::MilliSeconds(200));
+  }
+  actorSystem.Stop();
+  actorSystem.Cleanup();
+  return shouldContinue->GetReturnCode();
 }
