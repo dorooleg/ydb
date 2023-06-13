e511205ee04aa225cfc418e9674bf0a96af55051
Yranikus <96595573+Yranikus@users.noreply.github.com>
2023-06-13 19:55:02 +0300

    Update events.h

--- tools/actor_model/events.h
+++ tools/actor_model/events.h
@@ -1,6 +1,40 @@
 #include <library/cpp/actors/core/event_local.h>
 #include <library/cpp/actors/core/events.h>
+#include <stdint.h>
 
 struct TEvents {
+    enum EEv {
+        EvWriteValueRequest = EventSpaceBegin(NActors::TEvents::ES_USERSPACE +  1),
+        Wakeup, 
+        Poison,
+        Gone
+    };
+    
+    struct TEvWriteValueRequest : NActors::TEventLocal<TEvWriteValueRequest, EvWriteValueRequest>
+    {
+        int64_t Value;
+
+        TEvWriteValueRequest(int64_t &value)
+        : Value(value)
+        {}
+        
+    };
+    
+    struct TEvWakeup: public NActors::TEventBase<TEvWakeup, EEv::Wakeup> {
+            DEFINE_SIMPLE_LOCAL_EVENT(TEvWakeup, "System: TEvWakeup")
+
+            TEvWakeup(ui64 tag = 0) : Tag(tag) { }
+
+            const ui64 Tag = 0;
+    };
+
+    struct TEvPoisonPill : public NActors::TEventBase<TEvPoisonPill, EEv::Poison> {
+            DEFINE_SIMPLE_NONLOCAL_EVENT(TEvPoisonPill, "System: TEvPoison")
+    };
+
+    struct TEvDone: public NActors::TEventBase<TEvDone, EEv::Gone> {
+            DEFINE_SIMPLE_LOCAL_EVENT(TEvDone, "System: TEvGone")
+        };
+
     // Вам нужно самостоятельно сюда добавить все необходимые events в NActors::TEvents::ES_PRIVATE
 };
