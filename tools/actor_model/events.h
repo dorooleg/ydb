e609bfdb80c093b1160ead8ec72e01d80dbdd988
Yranikus <96595573+Yranikus@users.noreply.github.com>
2023-06-15 23:58:40 +0300

    fixed events.h

--- tools/actor_model/events.h
+++ tools/actor_model/events.h
@@ -1,40 +1,19 @@
 #include <library/cpp/actors/core/event_local.h>
 #include <library/cpp/actors/core/events.h>
-#include <stdint.h>
 
 struct TEvents {
-    enum EEv {
-        EvWriteValueRequest = EventSpaceBegin(NActors::TEvents::ES_USERSPACE +  1),
-        Wakeup, 
-        Poison,
-        Gone
-    };
-    
-    struct TEvWriteValueRequest : NActors::TEventLocal<TEvWriteValueRequest, EvWriteValueRequest>
-    {
-        int64_t Value;
-
-        TEvWriteValueRequest(int64_t &value)
-        : Value(value)
-        {}
-        
-    };
-    
-    struct TEvWakeup: public NActors::TEventBase<TEvWakeup, EEv::Wakeup> {
-            DEFINE_SIMPLE_LOCAL_EVENT(TEvWakeup, "System: TEvWakeup")
-
-            TEvWakeup(ui64 tag = 0) : Tag(tag) { }
-
-            const ui64 Tag = 0;
-    };
-
-    struct TEvPoisonPill : public NActors::TEventBase<TEvPoisonPill, EEv::Poison> {
-            DEFINE_SIMPLE_NONLOCAL_EVENT(TEvPoisonPill, "System: TEvPoison")
-    };
-
-    struct TEvDone: public NActors::TEventBase<TEvDone, EEv::Gone> {
-            DEFINE_SIMPLE_LOCAL_EVENT(TEvDone, "System: TEvGone")
-        };
-
     // Вам нужно самостоятельно сюда добавить все необходимые events в NActors::TEvents::ES_PRIVATE
+    
+    enum EEv {
+        EvDiscoveryResponse = EventSpaceBegin(NActors::TEvents::ES_PRIVATE),
+        EvDone
+    };
+
+    struct TEvDone : NActors::TEventLocal<TEvDone, EvDone> {};
+    
+    struct TEvWriteValueRequest : NActors::TEventLocal<TEvWriteValueRequest, EvDiscoveryResponse> {
+    	int64_t Value;
+    	explicit TEvWriteValueRequest(int64_t value) : Value(value) {}
+    };
 };
+
