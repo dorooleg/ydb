684bf43c594a0dbff0cebc60d93c0b6867446789
Yranikus <96595573+Yranikus@users.noreply.github.com>
2023-06-16 00:01:29 +0300

    fixed actors.h

--- tools/actor_model/actors.h
+++ tools/actor_model/actors.h
@@ -5,7 +5,7 @@
 
 THolder<NActors::IActor> CreateSelfPingActor(const TDuration& latency);
 THolder<NActors::IActor> CreateTReadActor(const NActors::TActorId writeActor);
-THolder<NActors::IActor> CreateTMaximumPrimeDevisorActor(int64_t value, const NActors::TActorIdentity readActor, const NActors::TActorId writeActor);
+THolder<NActors::IActor> CreateTMaximumPrimeDevisorActor(int64_t value, const NActors::TActorId readActor, const NActors::TActorId writeActor);
 THolder<NActors::IActor> CreateTWriteActor();
 
 std::shared_ptr<TProgramShouldContinue> GetProgramShouldContinue();
