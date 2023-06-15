815319e18629ca36a655290d01a9e7652c175cab
Alexandr <Alistan4@gmail.com>
2023-06-15 13:57:53 +0300

    my_own_version_done

--- tools/actor_model/actors.h
+++ tools/actor_model/actors.h
@@ -2,11 +2,23 @@
 #include <library/cpp/actors/core/actor.h>
 #include <util/generic/ptr.h>
 #include <library/cpp/actors/util/should_continue.h>
-#include "eratosphene_sieve.h"
+
 
 THolder<NActors::IActor> CreateSelfPingActor(const TDuration& latency);
-THolder<NActors::IActor> CreateSelfTReadActor(const NActors::TActorId writeActor, TheSieveOfEratosthenes* theSieveOfEratosthenes);
-THolder <NActors::IActor> CreateSelfTMaximumPrimeDevisorActor(int value, const NActors::TActorIdentity readActor, NActors::TActorId writeActor, eratosphene_sieve* eratosphene_sieve);
+THolder<NActors::IActor> CreateSelfTReadActor(NActors::TActorId writeActor);
+THolder <NActors::IActor> CreateSelfTMaximumPrimeDevisorActor(int value, NActors::TActorIdentity readActor, NActors::TActorId writeActor);
 THolder<NActors::IActor> CreateSelfTWriteActor();
 
 std::shared_ptr<TProgramShouldContinue> GetProgramShouldContinue();
+
+class checkIsPrimeNumberReturns{
+public:
+    bool status;
+    bool result;
+    int Obtained;
+
+    checkIsPrimeNumberReturns(bool status, bool result, int obtainedValue) : status(status), result(result),
+                                                                             Obtained(obtainedValue){};
+};
+
+checkIsPrimeNumberReturns* checkIsPrimeNumber(int n, auto startTime, int Obtained);
