dbca85386690e3b3d59f63d777206a805b9a9cda
Zirsar <71979562+Zirsar@users.noreply.github.com>
2023-06-15 20:22:04 +0300

    Update main.cpp

--- tools/actor_model/main.cpp
+++ tools/actor_model/main.cpp
@@ -3,31 +3,38 @@
 #include <library/cpp/actors/core/scheduler_basic.h>
 #include <util/generic/xrange.h>
 
-THolder<NActors::TActorSystemSetup> BuildActorSystemSetup(ui32 threads, ui32 pools) {
+THolder<NActors::TActorSystemSetup> BuildActorSystemSetup(ui32 threads, ui32 pools)
+{
     auto setup = MakeHolder<NActors::TActorSystemSetup>();
     setup->ExecutorsCount = pools;
     setup->Executors.Reset(new TAutoPtr<NActors::IExecutorPool>[pools]);
-    for (ui32 idx : xrange(pools)) {
+    for (ui32 idx : xrange(pools))
+    {
         setup->Executors[idx] = new NActors::TBasicExecutorPool(idx, threads, 512);
     }
     setup->Scheduler.Reset(new NActors::TBasicSchedulerThread(NActors::TSchedulerConfig(512, 0)));
     return setup;
 }
 
-int main(int argc, const char* argv[])
+int main(int argc, const char *argv[])
 {
     Y_UNUSED(argc, argv);
     auto actorySystemSetup = BuildActorSystemSetup(20, 1);
     NActors::TActorSystem actorSystem(actorySystemSetup);
     actorSystem.Start();
 
-    auto writeActor = actorSystem.Register(NActors::TActorFactory::Create<TWriteActor>().Release());
-    auto readActor = actorSystem.Register(NActors::TActorFactory::Create<TReadActor>(std::cin, writeActor->GetId()).Release());
+    actorSystem.Register(CreateSelfPingActor(TDuration::Seconds(1)).Release());
 
-auto shouldContinue = GetProgramShouldContinue();
-while (shouldContinue->PollState() == TProgramShouldContinue::Continue) {
-Sleep(TDuration::MilliSeconds(200));
-}
+    // Зарегистрируйте Write и Read акторы здесь
+    auto actorId = actorSystem.Register(CreateTWriteActor().Release());
+    actorSystem.Register(CreateTReadActor(actorId).Release());
+
+    // Раскомментируйте этот код
+    auto shouldContinue = GetProgramShouldContinue();
+    while (shouldContinue->PollState() == TProgramShouldContinue::Continue)
+    {
+        Sleep(TDuration::MilliSeconds(200));
+    }
     actorSystem.Stop();
     actorSystem.Cleanup();
     return shouldContinue->GetReturnCode();
