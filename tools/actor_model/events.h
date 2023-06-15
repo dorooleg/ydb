8e6eb82dd6d49e06eed33dc8868d66d64a259576
Zirsar <71979562+Zirsar@users.noreply.github.com>
2023-06-15 20:21:40 +0300

    Update events.h

--- tools/actor_model/events.h
+++ tools/actor_model/events.h
@@ -1,15 +1,22 @@
 #include <library/cpp/actors/core/event_local.h>
 #include <library/cpp/actors/core/events.h>
 
-struct TEvents {
-    enum ECustomEvents {
-        EvDiscoveryResponse = NActors::TEvents::ES_PRIVATE + 1,
-        EvDone
+struct TEvents
+{
+    // Вам нужно самостоятельно сюда добавить все необходимые events в NActors::TEvents::ES_PRIVATE
+
+    enum EEv
+    {
+        EvDone,
+        EvRequest = EventSpaceBegin(NActors::TEvents::ES_PRIVATE),
     };
 
-    struct TEvDone : NActors::TEventLocal<TEvDone, EvDone> {};
+    struct TEvDone : NActors::TEventLocal<TEvDone, EvDone>
+    {
+    };
 
-    struct TEvWriteValueRequest : NActors::TEventLocal<TEvWriteValueRequest, EvDiscoveryResponse> {
+    struct TEvWriteValueRequest : NActors::TEventLocal<TEvWriteValueRequest, EvRequest>
+    {
         int64_t Value;
         explicit TEvWriteValueRequest(int64_t value) : Value(value) {}
     };
