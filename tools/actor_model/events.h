ab10e03cf8fa15d91cd11c0e0e97a5200532a74b
Андрей Михайлов <55950615+Andrey1408@users.noreply.github.com>
2024-05-25 22:27:50 +0300

    Update events.h

--- tools/actor_model/events.h
+++ tools/actor_model/events.h
@@ -3,4 +3,14 @@
 
 struct TEvents {
     // Вам нужно самостоятельно сюда добавить все необходимые events в NActors::TEvents::ES_PRIVATE
+    enum events {
+        EvValue = EventSpaceBegin(NActors::TEvents::ES_PRIVATE),
+        Done
+    };
+
+    struct TEvWriteValueRequest : NActors::TEventLocal<TEvWriteValueRequest, EvValue> {
+        int64_t Value;
+        explicit TEvWriteValueRequest(int64_t value) : Value(value) {}
+    };
+    struct TEvDone : NActors::TEventLocal<TEvDone, Done> {};
 };
