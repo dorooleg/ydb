f1fe8d475a1f200309e3c95d6f0f81bd0ffc73b9
Ink-sta <52159105+Ink-sta@users.noreply.github.com>
2024-05-25 15:13:26 +0300

    edit 1 events.h

--- tools/actor_model/events.h
+++ tools/actor_model/events.h
@@ -3,4 +3,17 @@
 
 struct TEvents {
     // Вам нужно самостоятельно сюда добавить все необходимые events в NActors::TEvents::ES_PRIVATE
+    enum EEv {
+        EvDiscoveryResponse = EventSpaceBegin(NActors::TEvents::ES_PRIVATE),
+        EvEnd
+    };
+
+    struct TEvDone : NActors::TEventLocal<TEvDone, EvEnd> {};
+
+    struct TEvWriteValueRequest : NActors::TEventLocal<TEvWriteValueRequest, EvDiscoveryResponse> {
+        int64_t value;
+        TEvWriteValueRequest(int64_t value)
+                : value(value)
+        {}
+    };
 };
