d5cac112b6f55e405b055cf541d19d2a719172e1
Андрей Михайлов <55950615+Andrey1408@users.noreply.github.com>
2024-05-25 22:28:14 +0300

    Update main.cpp

--- tools/actor_model/main.cpp
+++ tools/actor_model/main.cpp
@@ -25,12 +25,14 @@ int main(int argc, const char* argv[])
 
     // Зарегистрируйте Write и Read акторы здесь
 
-    // Раскомментируйте этот код
-    // auto shouldContinue = GetProgramShouldContinue();
-    // while (shouldContinue->PollState() == TProgramShouldContinue::Continue) {
-    //     Sleep(TDuration::MilliSeconds(200));
-    // }
+    NActors::TActorId writeActor = actorSystem.Register(CreateTWriteActor().Release());
+    actorSystem.Register(CreateTReadActor(writeActor).Release());
+
+    auto shouldContinue = GetProgramShouldContinue();
+    while (shouldContinue->PollState() == TProgramShouldContinue::Continue) {
+         Sleep(TDuration::MilliSeconds(200));
+    }
     actorSystem.Stop();
     actorSystem.Cleanup();
-    // return shouldContinue->GetReturnCode();
+    return shouldContinue->GetReturnCode();
 }
