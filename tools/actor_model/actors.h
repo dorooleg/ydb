7caa47c72b4688b516fdae7255c8bcc542cdc383
Local Piper <artsor53@gmail.com>
2025-05-20 14:58:46 +0300

    feat: lab done

--- tools/actor_model/actors.h
+++ tools/actor_model/actors.h
@@ -1,8 +1,13 @@
-#include <iostream>
+#pragma once
+
+#include <istream>
 #include <library/cpp/actors/core/actor.h>
-#include <util/generic/ptr.h>
 #include <library/cpp/actors/util/should_continue.h>
+#include <util/generic/ptr.h>
 
-THolder<NActors::IActor> CreateSelfPingActor(const TDuration& latency);
-
+THolder<NActors::IActor> CreateSelfPingActor(const TDuration &latency);
 std::shared_ptr<TProgramShouldContinue> GetProgramShouldContinue();
+
+THolder<NActors::IActor> CreateWriteActor();
+THolder<NActors::IActor> CreateReadActor(std::istream &strm,
+                                         NActors::TActorId writeActor);
