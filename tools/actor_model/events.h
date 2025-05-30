06d9a2e624daacdae46a16eeca2f90629f7c1ae8
PrinceOfAllRuss <125488187+PrinceOfAllRuss@users.noreply.github.com>
2025-05-30 23:51:47 +0300

    Update events.h

--- tools/actor_model/events.h
+++ tools/actor_model/events.h
@@ -2,5 +2,18 @@
 #include <library/cpp/actors/core/events.h>
 
 struct TEvents {
-    // Вам нужно самостоятельно сюда добавить все необходимые events в NActors::TEvents::ES_PRIVATE
+    struct EventIdentifiers {
+        static constexpr int RESULT_EVENT = EventSpaceBegin(NActors::TEvents::ES_PRIVATE);
+        static constexpr int COMPLETION_EVENT = RESULT_EVENT + 1;
+    };
+
+    struct TWriteResultEvent : public NActors::TEventLocal<TWriteResultEvent, EventIdentifiers::RESULT_EVENT> {
+        int64_t ResultValue;
+
+        TWriteResultEvent(int64_t value) : ResultValue(value) {}
+    };
+
+    struct TComputationDone : public NActors::TEventLocal<TComputationDone, EventIdentifiers::COMPLETION_EVENT> {
+        static constexpr const char* DESCRIPTION = "ComputationDoneEvent";
+    };
 };
