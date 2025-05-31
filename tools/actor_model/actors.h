8c42e5e375884af966cc1a9dae50962ce89dd46f
Абдуллин Игорь <igor.adbullin.95@gmail.com>
2025-05-31 13:14:26 +0300

    Done lab

--- tools/actor_model/actors.h
+++ tools/actor_model/actors.h
@@ -3,6 +3,22 @@
 #include <util/generic/ptr.h>
 #include <library/cpp/actors/util/should_continue.h>
 
-THolder<NActors::IActor> CreateSelfPingActor(const TDuration& latency);
+THolder<NActors::IActor> CreateSelfPingActor(const TDuration &latency);
+THolder<NActors::IActor> CreateSelfTReadActor(NActors::TActorId writeActor);
+THolder<NActors::IActor> CreateSelfTMaximumPrimeDevisorActor(int value, NActors::TActorIdentity readActor, NActors::TActorId writeActor);
+THolder<NActors::IActor> CreateSelfTWriteActor();
 
 std::shared_ptr<TProgramShouldContinue> GetProgramShouldContinue();
+
+class checkIsPrimeNumberReturns
+{
+public:
+    bool status;
+    bool result;
+    int received;
+
+    checkIsPrimeNumberReturns(bool status, bool result, int receivedValue) : status(status), result(result),
+                                                                             received(receivedValue) {};
+};
+
+checkIsPrimeNumberReturns *checkIsPrimeNumber(int n, auto startTime, int received);
--- tools/actor_model/events.h
+++ tools/actor_model/events.h
@@ -1,6 +1,21 @@
 #include <library/cpp/actors/core/event_local.h>
 #include <library/cpp/actors/core/events.h>
 
-struct TEvents {
-    // Вам нужно самостоятельно сюда добавить все необходимые events в NActors::TEvents::ES_PRIVATE
+struct TEvents
+{
+    enum Events
+    {
+        EventResponse = EventSpaceBegin(NActors::TEvents::ES_PRIVATE),
+        EventDone
+    };
+
+    struct TEvDone : NActors::TEventLocal<TEvDone, EventDone>
+    {
+    };
+
+    struct TEvWriteValueRequest : NActors::TEventLocal<TEvWriteValueRequest, EventResponse>
+    {
+        int64_t value;
+        explicit TEvWriteValueRequest(int Value) : value(Value) {}
+    };
 };
