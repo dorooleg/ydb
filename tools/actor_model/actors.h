e790b4108a4250953416e5f260421747e5a6fc44
Kirill Zakusov <username918r818@outlook.com>
2025-05-31 22:05:39 +0300

    init

--- tools/actor_model/actors.h
+++ tools/actor_model/actors.h
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <library/cpp/actors/core/actor.h>
-#include <util/generic/ptr.h>
 #include <library/cpp/actors/util/should_continue.h>
+#include <util/generic/ptr.h>
 
-THolder<NActors::IActor> CreateSelfPingActor(const TDuration& latency);
+THolder<NActors::IActor> CreateSelfPingActor(const TDuration &latency);
+THolder<NActors::IActor> CreateReadActor(IInputStream &strm,
+                                         const NActors::TActorId &writeActorId);
+THolder<NActors::IActor>
+CreateMaximumPrimeDivisorActor(int64_t value,
+                               const NActors::TActorId &readActorId,
+                               const NActors::TActorId &writeActorId);
+THolder<NActors::IActor> CreateWriteActor();
 
 std::shared_ptr<TProgramShouldContinue> GetProgramShouldContinue();
